4f1b1c4faba9b37010e2dec5ada68a7fc325293f
bobw@prophecy.io <bobw@prophecy.io>
2024-09-23 18:32:18 +0000

    Updates metadata and descriptions for multiple datasets and pipelines - Changes 'id' field type in 'all_vectors_silver' dataset - Adds descriptions to fields in 'web_bronze_sitemap' and 'web_bronze_url' datasets - Updates dependencies and configurations in 'web_ingest' and 'web_vectorize' pipelines

--- pipelines/web_ingest/code/web_ingest/pipeline.py
+++ pipelines/web_ingest/code/web_ingest/pipeline.py
@@ -1,7 +1,6 @@
 from pyspark.sql import *
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
-from prophecy.utils import *
 from web_ingest.config.ConfigStore import *
 from web_ingest.udfs.UDFs import *
 from prophecy.utils import *
@@ -22,15 +21,12 @@ def main():
                 .config("spark.sql.legacy.allowUntypedScalaUDF", "true")\
                 .enableHiveSupport()\
                 .appName("Prophecy Pipeline")\
-                .getOrCreate()\
-                .newSession()
+                .getOrCreate()
     Utils.initializeFromArgs(spark, parse_args())
     spark.conf.set("prophecy.metadata.pipeline.uri", "pipelines/web_ingest")
     registerUDFs(spark)
     
-    MetricsCollector.start(spark = spark, pipelineId = "pipelines/web_ingest")
-    pipeline(spark)
-    MetricsCollector.end(spark)
+    MetricsCollector.instrument(spark = spark, pipelineId = "pipelines/web_ingest", config = Config)(pipeline)
 
 if __name__ == "__main__":
     main()
--- pipelines/web_vectorize/code/web_vectorize/pipeline.py
+++ pipelines/web_vectorize/code/web_vectorize/pipeline.py
@@ -1,7 +1,6 @@
 from pyspark.sql import *
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
-from prophecy.utils import *
 from web_vectorize.config.ConfigStore import *
 from web_vectorize.udfs.UDFs import *
 from prophecy.utils import *
@@ -25,15 +24,12 @@ def main():
                 .config("spark.sql.legacy.allowUntypedScalaUDF", "true")\
                 .enableHiveSupport()\
                 .appName("Prophecy Pipeline")\
-                .getOrCreate()\
-                .newSession()
+                .getOrCreate()
     Utils.initializeFromArgs(spark, parse_args())
     spark.conf.set("prophecy.metadata.pipeline.uri", "pipelines/web_vectorize")
     registerUDFs(spark)
     
-    MetricsCollector.start(spark = spark, pipelineId = "pipelines/web_vectorize")
-    pipeline(spark)
-    MetricsCollector.end(spark)
+    MetricsCollector.instrument(spark = spark, pipelineId = "pipelines/web_vectorize", config = Config)(pipeline)
 
 if __name__ == "__main__":
     main()
